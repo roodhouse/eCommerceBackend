b1f698f5b01612391c19aa4934b9610c815e9047
roodhouse <rughjm@gmail.com>
2022-10-28 13:21:17 -0500

    tag/product model

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,15 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    }
   },
   {
     sequelize,
@@ -18,3 +26,20 @@ Tag.init(
 );
 
 module.exports = Tag;
+
+
+// * `Tag`
+
+//   * `id`
+  
+//     * Integer.
+  
+//     * Doesn't allow null values.
+  
+//     * Set as primary key.
+  
+//     * Uses auto increment.
+
+//   * `tag_name`
+  
+//     * String.
