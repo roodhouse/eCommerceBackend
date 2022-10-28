6e846b0c3fa05c60b720497072f2d9231abf1c73
roodhouse <rughjm@gmail.com>
2022-10-28 14:01:58 -0500

    began product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,12 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll();
+    include: [{ model: Category, through: ProductTag, as: 'category'}];
+    include: [{ model: Tag, through: ProductTag, as: 'tag'}];
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
-// get one product
+// get one product -- here 
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
