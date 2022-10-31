9f56bb753686698b78326dfc436a449401d6ca1e
roodhouse <rughjm@gmail.com>
2022-10-31 11:33:08 -0500

    updated code to get post new category to work

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,19 +31,13 @@ router.get('/:id', async (req, res) => {
 });
 
 // create a new category
-router.post('/', (req, res) => {
-  const newCategory = 
-  {
-    category_name: "test",
-  }
-  Category.create(newCategory)
-  .then((category) => {
-    res.status(200).json(category);
-  })
-  .catch((err) => {
-    console.log(err);
+router.post('/', async (req, res) => {
+  try {
+  const newCategory = await Category.create(req.body);
+  res.status(200).json(newCategory);
+} catch(err) {
     res.status(400).json(err);
-  });
+  };
 });
 
 // delete a category by its `id` value
